8ede01ff0e7e599dbe59270f67ad0195e9f3de0b
David Richards <david.richards@cambridgemechatronics.com>
2020-03-21 23:09:50 +0000

    Added simple UDP broadcast

--- MusterMainFrm.Designer.cs
+++ MusterMainFrm.Designer.cs
@@ -28,42 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.Go = new System.Windows.Forms.Button();
-            this.roundsTimer = new System.Windows.Forms.Timer(this.components);
+            this.GoBells = new System.Windows.Forms.Button();
+            this.holePunchIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.GoInternet = new System.Windows.Forms.Button();
+            this.holePunchPort = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // Go
+            // GoBells
             // 
-            this.Go.Location = new System.Drawing.Point(316, 136);
-            this.Go.Name = "Go";
-            this.Go.Size = new System.Drawing.Size(75, 23);
-            this.Go.TabIndex = 0;
-            this.Go.Text = "Go";
-            this.Go.UseVisualStyleBackColor = true;
-            this.Go.Click += new System.EventHandler(this.Go_Click);
+            this.GoBells.Location = new System.Drawing.Point(316, 136);
+            this.GoBells.Name = "GoBells";
+            this.GoBells.Size = new System.Drawing.Size(75, 23);
+            this.GoBells.TabIndex = 0;
+            this.GoBells.Text = "Go Bells";
+            this.GoBells.UseVisualStyleBackColor = true;
+            this.GoBells.Click += new System.EventHandler(this.Go_Click);
             // 
-            // roundsTimer
+            // holePunchIP
             // 
-            this.roundsTimer.Interval = 271;
-            this.roundsTimer.Tick += new System.EventHandler(this.RoundsTimer_Tick);
+            this.holePunchIP.Location = new System.Drawing.Point(189, 229);
+            this.holePunchIP.Name = "holePunchIP";
+            this.holePunchIP.Size = new System.Drawing.Size(100, 22);
+            this.holePunchIP.TabIndex = 1;
+            this.holePunchIP.Text = "63.35.216.228";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 229);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "HolePunch";
+            // 
+            // GoInternet
+            // 
+            this.GoInternet.Location = new System.Drawing.Point(189, 277);
+            this.GoInternet.Name = "GoInternet";
+            this.GoInternet.Size = new System.Drawing.Size(100, 23);
+            this.GoInternet.TabIndex = 3;
+            this.GoInternet.Text = "Go Internet";
+            this.GoInternet.UseVisualStyleBackColor = true;
+            this.GoInternet.Click += new System.EventHandler(this.GoInternet_Click);
+            // 
+            // holePunchPort
+            // 
+            this.holePunchPort.Location = new System.Drawing.Point(189, 249);
+            this.holePunchPort.Name = "holePunchPort";
+            this.holePunchPort.Size = new System.Drawing.Size(100, 22);
+            this.holePunchPort.TabIndex = 4;
+            this.holePunchPort.Text = "51243";
             // 
             // Muster
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.Go);
+            this.Controls.Add(this.holePunchPort);
+            this.Controls.Add(this.GoInternet);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.holePunchIP);
+            this.Controls.Add(this.GoBells);
             this.Name = "Muster";
             this.Text = "Muster";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button Go;
-        private System.Windows.Forms.Timer roundsTimer;
+        private System.Windows.Forms.Button GoBells;
+        private System.Windows.Forms.TextBox holePunchIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button GoInternet;
+        private System.Windows.Forms.TextBox holePunchPort;
     }
 }
 
--- MusterMainFrm.cs
+++ MusterMainFrm.cs
@@ -6,6 +6,8 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Media;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -34,7 +36,7 @@ namespace Muster
         private void Go_Click(object sender, EventArgs e)
         {
             nextBell = 0;
-            roundsTimer.Enabled = true;
+            //roundsTimer.Enabled = true;
         }
 
         private void RoundsTimer_Tick(object sender, EventArgs e)
@@ -51,6 +53,18 @@ namespace Muster
                 bellSamples[nextBell % numberOfBells].Play();
                 nextBell = nextBell + 1;
             }
+        }
+
+        private Socket _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        private void GoInternet_Click(object sender, EventArgs e)
+        {            
+            _socket.Connect(IPAddress.Parse(holePunchIP.Text), int.Parse(holePunchPort.Text));
+
+            byte[] data = Encoding.ASCII.GetBytes("TestString");
+
+            var sent = _socket.Send(data);                                
+
         }
     }
 }
